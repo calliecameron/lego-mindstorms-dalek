7ac124b77eab1903259466406e6d4b5613f1e9e2
Callum Cameron <cjcameron7@gmail.com>
2014-10-05 09:59:38 +0100

    Started writing code.

--- code/dalek.py
+++ code/dalek.py
@@ -1,5 +1,6 @@
 """Dalek control classes."""
 
+import time
 from ev3.lego import LargeMotor, MediumMotor
 
 DRIVE_SPEED = -700
@@ -13,9 +14,9 @@ class Dalek(object):
         self.sounds_dir = sounds_dir
         self.left_wheel = LargeMotor("D")
         self.right_wheel = LargeMotor("A")
-        self.head_motor = MediumMotor("B")
         self.init_wheel(self.left_wheel)
         self.init_wheel(self.right_wheel)
+        self.head = Head()
 
     def init_wheel(self, wheel):
         wheel.reset()
@@ -30,14 +31,14 @@ class Dalek(object):
         else:
             return factor
 
-    def forward(self, factor):
+    def forward(self, factor=1.0):
         factor = self.clamp_percent(factor)
         self.left_wheel.pulses_per_second_sp = factor * DRIVE_SPEED
         self.right_wheel.pulses_per_second_sp = factor * DRIVE_SPEED
         self.left_wheel.start()
         self.right_wheel.start()
 
-    def reverse(self, factor):
+    def reverse(self, factor=1.0):
         factor = self.clamp_percent(factor)
         self.left_wheel.pulses_per_second_sp = factor * -DRIVE_SPEED
         self.right_wheel.pulses_per_second_sp = factor * -DRIVE_SPEED
@@ -47,3 +48,29 @@ class Dalek(object):
     def stop(self):
         self.left_wheel.stop()
         self.right_wheel.stop()
+
+class Head(object):
+    """Dalek's head."""
+
+    def __init__(self):
+        super(Head, self).__init__()
+        self.head_motor = MediumMotor("B")
+
+    def initialise(self):
+        self.head_motor.reset()
+        self.head_motor.regulation_mode = "off"
+        self.head_motor.stop_mode = "coast"
+        self.head_motor.duty_cycle_sp = 75
+        time.sleep(2)
+        pos1 = self.head_motor.position
+        self.head_motor.duty_cycle_sp = -75
+        time.sleep(2)
+        pos2 = self.head_motor.position
+        midpoint = (pos1 + pos2) / 2.0
+
+        self.head_motor.regulation_mode = "on"
+        self.head_motor.stop_mode = "hold"
+        self.head_motor.ramp_up_sp = 500
+        self.head_motor.ramp_down_sp = 200
+        self.head_motor.run_position_limited(midpoint, 400)
+        self.head_motor.position = 0
