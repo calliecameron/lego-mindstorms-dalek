41446347d9cce5534c576a2a0f7ad3c08d77542f
Callum Cameron <cjcameron7@gmail.com>
2014-10-10 16:08:27 +0100

    Implementing emergency stop and better controls.

--- code/dalek.py
+++ code/dalek.py
@@ -8,6 +8,12 @@ TICK_LENGTH_SECONDS = 0.1
 DRIVE_SPEED = -700
 TURN_SPEED = -500
 
+DRIVE_STOP = 0
+DRIVE_FORWARD = 1
+DRIVE_REVERSE = 2
+DRIVE_LEFT = 3
+DRIVE_RIGHT = 4
+
 def clamp_percent(factor):
     factor = float(factor)
     if factor < 0.0:
@@ -89,53 +95,89 @@ class Drive(EventQueue):
         self.left_wheel = init_wheel("D")
         self.right_wheel = init_wheel("A")
         self.touch_sensor = TouchSensor(2)
+        self.drive_state = DRIVE_STOP
+        self.drive_factor = 0.0
+        self.ticks_since_last = 0
 
-    def drive_action(self, speed):
+    def drive_action(self, speed, state, factor):
         def action():
+            self.drive_state = state
+            self.drive_factor = factor
             self.left_wheel.pulses_per_second_sp = speed
             self.right_wheel.pulses_per_second_sp = speed
             self.left_wheel.start()
             self.right_wheel.start()
+            self.ticks_since_last = 0
         return action
 
-    def turn_action(self, left_speed, right_speed):
+    def turn_action(self, left_speed, right_speed, state, factor):
         def action():
+            self.drive_state = state
+            self.drive_factor = factor
             self.left_wheel.pulses_per_second_sp = left_speed
             self.right_wheel.pulses_per_second_sp = right_speed
             self.left_wheel.start()
             self.right_wheel.start()
+            self.ticks_since_last = 0
         return action
 
     def stop_action(self):
         def action():
+            self.drive_state = DRIVE_STOP
+            self.drive_factor = 0.0
             self.left_wheel.stop()
             self.right_wheel.stop()
+            self.ticks_since_last = 0
+        return action
+
+    def conditional_stop_action(self, state):
+        def action():
+            if self.drive_state == state:
+                self.replace(self.stop_action())
         return action
 
     def pre_process(self):
         if self.touch_sensor.is_pushed:
             self.shutdown()
+        self.ticks_since_last += 1
+
+    def post_process(self):
+        if self.ticks_since_last > 75:
+            self.shutdown()
 
     def forward(self, factor=1.0):
-        self.replace(self.drive_action(clamp_percent(factor) * DRIVE_SPEED))
+        self.replace(self.drive_action(clamp_percent(factor) * DRIVE_SPEED, DRIVE_FORWARD, clamp_percent(factor)))
 
     def reverse(self, factor=1.0):
-        self.replace(self.drive_action(clamp_percent(factor) * -DRIVE_SPEED))
+        self.replace(self.drive_action(clamp_percent(factor) * -DRIVE_SPEED, DRIVE_REVERSE, clamp_percent(factor)))
 
     def turn_left(self, factor=1.0):
         factor = clamp_percent(factor)
-        self.replace(self.turn_action(factor * -TURN_SPEED, factor * TURN_SPEED))
+        self.replace(self.turn_action(factor * -TURN_SPEED, factor * TURN_SPEED, DRIVE_LEFT, factor))
 
     def turn_right(self, factor=1.0):
         factor = clamp_percent(factor)
-        self.replace(self.turn_action(factor * TURN_SPEED, factor * -TURN_SPEED))
+        self.replace(self.turn_action(factor * TURN_SPEED, factor * -TURN_SPEED, DRIVE_RIGHT, factor))
 
     def stop(self):
         self.replace(self.stop_action())
 
+    def forward_stop(self):
+        self.add(self.conditional_stop_action(DRIVE_FORWARD))
+
+    def reverse_stop(self):
+        self.add(self.conditional_stop_action(DRIVE_REVERSE))
+
+    def left_stop(self):
+        self.add(self.conditional_stop_action(DRIVE_LEFT))
+
+    def right_stop(self):
+        self.add(self.conditional_stop_action(DRIVE_RIGHT))
+
     def shutdown(self):
         self.clear()
         self.stop_action()()
+
 
 class ControllerThread(threading.Thread):
     def __init__(self, parent):
