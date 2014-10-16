e4ee2c591cc40a36761b4b8e67704301b1af627b
Callum Cameron <cjcameron7@gmail.com>
2014-10-16 20:05:35 +0100

    Remote control.

--- code/remote_receiver.py
+++ code/remote_receiver.py
@@ -14,16 +14,23 @@ class DalekReceiver(Receiver):
 
     def begin_cmd(self, cmd, factor):
         if cmd == FORWARD:
+            print "baz1"
             self.d.drive.forward(factor)
+            print "baz2"
         elif cmd == REVERSE:
+            print "bar1"
             self.d.drive.reverse(factor)
+            print "bar2"
 
     def release_cmd(self, cmd):
         if cmd == FORWARD:
+            print "foo1"
             self.d.drive.forward_stop()
+            print "foo2"
         elif cmd == REVERSE:
-            print "blargh"
+            print "blargh1"
             self.d.drive.reverse_stop()
+            print "blargh2"
 
     def stop(self):
         self.d.drive.stop()
